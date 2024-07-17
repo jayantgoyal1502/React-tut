c4eb1a84931676bfd2382602ea78608531431b5d
jayantgoyal1502 <jayantgoyal500@gmail.com>
2024-07-18 02:27:01 +0530

    counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter ,setCounter] = useState(0)
+
+
+  // let counter  = 5;
+
+  const addValue = () => {
+    if(counter<20){
+      counter = counter+1
+      setCounter(counter)
+    }
+  }
+
+  const removeValue = ()=>{
+    if(counter>0){
+      counter = counter-1
+      setCounter(counter)
+    }
+  }
+
+  return (
+    <>
+      <h1>Counter Project</h1>
+      <h2>Counter value : {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add Value</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >Remove value</button>
+    </>
+  )
+}
+
+export default App
